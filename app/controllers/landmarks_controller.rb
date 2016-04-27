551fab642d193a9cf1e2250e0995570dc9104729
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-04-27 15:18:16 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,31 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :"landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    Landmark.create(params["landmark"])
+    erb :"/landmarks/index"
+  end 
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    erb :'/landmarks/#{@landmark.id}'
+  end
 end
---
+++ db/migrate/20160427183831_add_columns_to_landmarks.rb
@@ -0,0 +1,5 @@
+class AddColumnsToLandmarks < ActiveRecord::Migration
+  def change
+    add_column :landmarks, :year_completed, :integer
+  end
+end
