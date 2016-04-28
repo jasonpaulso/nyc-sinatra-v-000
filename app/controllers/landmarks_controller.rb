7d0026decf4bcc540d4341cb44b0a438e6b7316d
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-04-27 19:26:45 -0700

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,16 +1,25 @@
 class LandmarksController < ApplicationController
 
-  get('/landmarks') {erb :"landmarks/index"}
+  get ('/landmarks') {erb :"landmarks/index"}
 
   get('/landmarks/new') {erb :"landmarks/new"}
 
   get('/landmarks/:id') {@landmark = Landmark.find_by_id(params[:id]);erb :'landmarks/show'}
 
-  post('/landmarks') {Landmark.create(params["landmark"]);erb :"/landmarks/index"}
+  post '/landmarks' do
+    Landmark.create(params["landmark"])
+    erb :"/landmarks/index"
+  end 
 
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'landmarks/edit'
+  end
 
-  get('/landmarks/:id/edit'){@landmark = Landmark.find_by_id(params[:id]);erb :'landmarks/edit'} 
-
-  post('/landmarks/:id'){@landmark = Landmark.find_by_id(params[:id]);@landmark.update(params[:landmark]);@landmark.save;erb :'/landmarks/#{@landmark.id}'}
-  
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+    erb :'/landmarks/#{@landmark.id}'
+  end
 end
